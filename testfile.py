d2c8a00d414bb410b41ecaf20cb91b7aea988800
mamatagupta2026 <mgupta@archaea-ai.com>
2026-03-17 12:21:50 +0530

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -6,4 +6,4 @@ try:
     print(f"The sum of {num1} and {num2} is {total}")
 except ValueError:
     print("Invalid input! Please enter numeric values.")
-    print("Invalid input! Please enter new value .")
+    print("Invalid input.")
