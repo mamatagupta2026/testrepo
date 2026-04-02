977470a833d1877ca1500bb3820c22531d249512
mamatagupta2026 <mgupta@archaea-ai.com>
2026-04-02 18:14:03 +0530

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,6 +1,6 @@
 # Program to add two numbers with input validation
 try:
-    num1 = float(input("Enter first number: "))
+    num1 = float(input("Enter first number here : "))
     num2 = float(input("Enter second number: "))
     total = num1 + num2
     print(f"The sum of {num1} and {num2} is {total}")
